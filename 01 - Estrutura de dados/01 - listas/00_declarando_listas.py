9fb3eb8de948f36c9fa8cca5f78307f182b2af1a
Bruno <brunno.luccas.22@gmail.com>
2026-03-06 10:44:46 -0400

    Adicionando 00_declarando_listas.py

---
+++ 01 - Estrutura de dados/01 - listas/00_declarando_listas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "uva", "limão siciliano"]
+print(frutas)
+
+frutas= []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carros = ["Ferrari", "F8", 4200000, 2020, 290, "São Paulo", True]
+print(carros)
